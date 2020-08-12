2fff1d429f60db536744ae32f08a7d43cd9fd068
Akaitatsu <bryant@mindfair.com>
2020-08-12 16:13:50 -0400

    Merged Copy Mods into Install Instances process

--- build/Copy-Mods.ps1
+++ build/Copy-Mods.ps1
@@ -1,6 +1,7 @@
-Set-Location $PSScriptRoot
-$instances = @("LCMS", "WFMS", "GWMS")
-foreach ($instance in $instances) {
-    $targetPath = "..\..\MultiMC\instances\$instance\.minecraft\mods"
-    Copy-Item -Path "..\modcache\*.jar" -Destination $targetPath
-}
+param (
+    [Parameter(Mandatory=$True)]$instanceObject    
+)
+Write-Host "  Copying Mods"
+$targetModsPath = "..\..\MultiMC\instances\$($instanceObject.shortName)\.minecraft\mods"
+Copy-Item -Path "..\modcache\*.jar" -Destination $targetModsPath
+Write-Host "  Copied Mods"
--- build/Install-Instances.ps1
+++ build/Install-Instances.ps1
@@ -5,6 +5,7 @@ foreach ($instance in $instances) {
     Write-Host ("Installing Instance {0} ({1})" -f $instance.name, $instance.shortName)
     .\Build-InstanceCfgFile.ps1 -instanceObject $instance
     .\Copy-StaticInstanceFiles.ps1 -instanceObject $instance
+    .\Copy-Mods.ps1 -instanceObject $instance
     Write-Host ("Installed Instance {0} ({1})" -f $instance.name, $instance.shortName)
     Write-Host ""
 }
