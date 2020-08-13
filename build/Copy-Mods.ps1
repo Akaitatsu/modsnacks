0a53df40b2b61d88e98f2d60163fa0098a0b42b7
Akaitatsu <bryant@mindfair.com>
2020-08-13 18:31:43 -0400

    Added dynamic mod copy logic

--- build/Copy-Mods.ps1
+++ build/Copy-Mods.ps1
@@ -1,8 +1,34 @@
 param (
-    [Parameter(Mandatory=$True)]$instanceObject    
+    [Parameter(Mandatory=$True)]$instanceObject,
+    [Parameter(Mandatory=$True)]$allInstances
 )
+Import-Module .\ModCache.psm1 -Force
+
+function Copy-ModsInList {
+    param (
+        $modsArray,
+        [string]$destinationPath
+    )
+    foreach ($mod in $modsArray) {
+        Copy-ModFromCache $mod $destinationPath
+    }
+}
+
 Write-Host "  Copying Mods"
-$targetModsPath = "..\..\MultiMC\instances\$($instanceObject.shortName)\.minecraft\mods"
-Remove-Item ($targetModsPath + "\*.jar")
-Copy-Item -Path "..\modcache\*.jar" -Destination $targetModsPath
+$targetModsPath = "..\..\MultiMC\instances\$($instanceObject.shortName)\.minecraft\mods\"
+Remove-Item ($targetModsPath + "*.jar")
+foreach ($currentInstance in $allInstances) {
+    # Copy LCMS mods to current instance
+    if ($currentInstance.shortName -eq "LCMS") {
+        Copy-ModsInList $currentInstance.mods $targetModsPath
+    }
+    # If installing WFMS or GWMS, copy WFMS mods to current instance
+    if ($currentInstance.shortName -eq "WFMS" -and ($instanceObject.shortName -eq "WFMS" -or $instanceObject.shortName -eq "GWMS")) {
+        Copy-ModsInList $currentInstance.mods $targetModsPath
+    }
+    # If installing GWMS, copy GWMS mods to current instance
+    if ($currentInstance.shortName -eq "GWMS" -and $instanceObject.shortName -eq "GWMS") {
+        Copy-ModsInList $currentInstance.mods $targetModsPath
+    }
+}
 Write-Host "  Copied Mods"
--- build/ModCache.psm1
+++ build/ModCache.psm1
@@ -1,15 +1,22 @@
 # Verify that all the mods defined in the instance configuration have been downloaded to the modcache folder
+function Get-ModFilename {
+    param ($modObject)
+    return $modObject.filenamePattern.Replace("%ver%", $modObject.currentVersion) + ".jar"
+}
+function Get-ModFilePath {
+    param ($modObject)
+    return "..\modcache\" + (Get-ModFilename $modObject)
+}
 function Test-ModCache {
     param (
         [Parameter(Mandatory=$True)]$allInstances
     )
-    $modcachePath = "..\modcache\"
     $totalModCount = 0
     $foundModCount = 0
     foreach ($instance in $allInstances) {
         foreach ($mod in $instance.mods) {
-            $modFilename = $mod.filenamePattern.Replace("%ver%", $mod.currentVersion) + ".jar"
-            $modPath = ($modcachePath + $modFilename)
+            $modFilename = Get-ModFilename $mod
+            $modPath = Get-ModFilePath $mod
             $totalModCount++
             if (Test-Path $modPath) {
                 $foundModCount++
@@ -20,4 +27,15 @@ function Test-ModCache {
         }
     }
     return ($foundModCount -eq $totalModCount)
+}
+
+function Copy-ModFromCache {
+    param (
+        $modObject,
+        [string]$destinationPath
+    )
+    $sourcePath = Get-ModFilePath $modObject
+    #$destinationFilePath = $destinationPath + (Get-ModFilename $modObject)
+    $destinationFilePath
+    Copy-Item -Path $sourcePath -Destination $destinationPath
 }
