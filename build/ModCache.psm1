6c97b960a77a31e48e7a0eccf493953c72c105cf
Akaitatsu <bryant@mindfair.com>
2020-10-04 17:57:52 -0400

    Script reorg

--- build/ModCache.psm1
+++ build/ModCache.psm1
@@ -32,17 +32,6 @@ function Test-ModCache {
     }
     return ($foundModCount -eq $totalModCount)
 }
-function Copy-ModConfig {
-    param (
-        $modObject,
-        [string]$destinationPath
-    )
-    $sourceFolder = "..\packdata\config\$($modObject.modid)"
-    $sourcePath = "$sourceFolder\*"
-    if (Test-Path $sourceFolder -PathType Container) {
-        Copy-Item -Path $sourcePath -Destination $destinationPath -Recurse
-    }
-}
 function Copy-ModFromCache {
     param (
         $modObject,
