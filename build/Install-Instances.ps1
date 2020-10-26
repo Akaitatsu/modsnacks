1f3d458f689fb6582b404c22a55d31156cfdd306
Akaitatsu <bryant@mindfair.com>
2020-10-25 21:00:42 -0400

    Added Random Patches config

--- build/Install-Instances.ps1
+++ build/Install-Instances.ps1
@@ -5,6 +5,7 @@ Import-Module .\Utilities.psm1 -Force
 Import-Module .\ModCache.psm1 -Force
 Clear-Host
 Set-Location $PSScriptRoot
+$modSnackInfo = Get-JsonAsPSCustomObject -Path ..\packdata\modsnacks.json
 $instances = Get-JsonAsPSCustomObject -Path ..\packdata\instances.json
 Write-Host "Verifying Mod Cache"
 if (-not (Test-ModCache)) {
@@ -14,12 +15,14 @@ if (-not (Test-ModCache)) {
 Write-Host "Verified Mod Cache"
 Import-Module .\dynamicconfig\Create.psm1 -Force
 Import-Module .\dynamicconfig\Druidcraft.psm1 -Force
+Import-Module .\dynamicconfig\RandomPatches.psm1 -Force
 foreach ($instance in $instances) {
     Write-Host ("Installing Instance {0} ({1})" -f $instance.name, $instance.shortName)
     .\Build-InstanceCfgFile.ps1 -instanceObject $instance
     .\Copy-StaticInstanceFiles.ps1 -instanceObject $instance
     $instanceMods = Get-Mods | Where-Object { $instance.includeInstanceMods.Contains($_.firstInstance) }
     $minecraftPath = "..\..\MultiMC\instances\$($instance.shortName)\.minecraft"
+    $configPath = "$minecraftPath\config"
     if (-not $SkipModCopy) {
         .\Copy-Mods.ps1 -InstanceObject $instance -InstanceMods $instanceMods -MinecraftPath $minecraftPath
     }
@@ -30,6 +33,7 @@ foreach ($instance in $instances) {
     $modList = $instanceMods.modid
     New-CreateConfig -modList $modList -openloaderPath $targetOpenLoaderPath
     New-DruidcraftConfig -modList $modList -openloaderPath $targetOpenLoaderPath
+    New-RandomPatchesConfig -ModSnackVersion $modSnackInfo.modSnackVersion -InstanceInfo $instance -ConfigPath $configPath
     Write-Host "  Generated dynamic configuration files"
     Write-Host ("Installed Instance {0} ({1})" -f $instance.name, $instance.shortName)
     Write-Host ""
