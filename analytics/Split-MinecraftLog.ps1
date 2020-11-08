16140d31a177caa12a7ad730e9cf420232718baa
Akaitatsu <bryant@mindfair.com>
2020-11-07 22:06:49 -0500

    Created script to split log files by mod for #25

---
+++ analytics/Split-MinecraftLog.ps1
@@ -0,0 +1,196 @@
+param (
+    [string]$LogFilePath = "D:\MultiMC\instances\GWMS\.minecraft\logs\latest.log",
+    [string]$DestinationDirectory = ""
+)
+
+if (-not (Test-Path $LogFilePath)) {
+    Write-Host "Specified file does not exist." -ForegroundColor Red
+    Write-Host "  $LogFilePath" -ForegroundColor Red
+}
+if ("" -eq $DestinationDirectory) {
+    $DestinationDirectory = "$PSScriptRoot\splitlogs"
+}
+if (Test-Path "$DestinationDirectory\*.log") {
+    Remove-Item "$DestinationDirectory\*.log" -Force
+}
+
+#Populate known mod name/id mappings
+$modIdMapping = @{
+    "AE2:C" = "appliedenergistics2";
+    "ambientsounds" = "ambientsounds";
+    "Apotheosis : Enchantment" = "apotheosis";
+    "Astral Sorcery" = "astralsorcery";
+    "attained_drops" = "attained_drops";
+    "bettercaves" = "bettercaves";
+    "BonsaiTrees2" = "bonsaitrees";
+    "Bookshelf" = "bookshelf";
+    "Botany Pots" = "botanypots";
+    "charm" = "charm";
+    "com.black_dog20.servertabinfo.ServerTabInfo" = "servertabinfo";
+    "com.darkere.serverconfigupdater.ServerConfigUpdater" = "serverconfigupdater";
+    "com.jaquadro.minecraft.storagedrawers.StorageDrawers" = "storagedrawers";
+    "com.maciej916.maessentials.common.util.LogUtils" = "maessentials";
+    "com.mojang.text2speech.NarratorWindows" = "minecraft";
+    "com.oitsjustjose.vtweaks.VTweaks" = "vtweaks";
+    "com.performant.coremod.Performant" = "performant";
+    "com.polyvalord.extlights.Extlights" = "extlights";
+    "com.refinedmods.refinedstorage.apiimpl.API" = "refinedstorage";
+    "com.teamabnormals.abnormals_core.core.AbnormalsCore" = "abnormalscore";
+    "covalent" = "covalent";
+    "cpw.mods.modlauncher.Launcher" = "misc";
+    "cpw.mods.modlauncher.LaunchServiceHandler" = "misc";
+    "Cucumber Library" = "cucumber";
+    "Dark Paintings" = "darkpaintings";
+    "Dark Utilities" = "darkutils";
+    "doubleslabs" = "doubleslabs";
+    "druidcraft" = "druidcraft";
+    "Equipment Tooltips" = "equipmenttooltips";
+    "FluxNetworks" = "fluxnetworks";
+    "immersiveengineering" = "immersiveengineering";
+    "incontrol" = "incontrol";
+    "inspirations" = "inspirations";
+    "inventoryhud" = "inventoryhud";
+    "JarSignVerifier" = "misc";
+    "jeresources" = "jeresources";
+    "Kiwi" = "kiwi";
+    "KubeJS" = "kubejs";
+    "KubeJS Startup" = "kubejs";
+    "lostcities" = "lostcities";
+    "mcjtylib" = "mcjtylib";
+    "mcwbridges" = "mcwbridges";
+    "mcwdoors" = "mcwdoors";
+    "mcwfurnitures" = "mcwfurnitures";
+    "mcwwindows" = "mcwwindows";
+    "Mekanism" = "mekanism";
+    "Meson" = "misc";
+    "mezz.jei.ingredients.IngredientManager" = "jei";
+    "mezz.jei.load.PluginCaller" = "jei";
+    "mezz.jei.load.registration.AdvancedRegistration" = "jei";
+    "mezz.jei.plugins.vanilla.ingredients.item.ItemStackListFactory" = "jei";
+    "mezz.jei.recipes.RecipeManager" = "jei";
+    "mezz.jei.util.LoggedTimer" = "jei";
+    "minecolonies" = "minecolonies";
+    "minecolonies.requestsystem" = "minecolonies";
+    "mixin" = "mixinbootstrap";
+    "Mystical Agriculture" = "mysticalagriculture";
+    "net.blay09.mods.craftingtweaks.CraftingTweaks" = "craftingtweaks";
+    "net.minecraft.advancements.AdvancementList" = "minecraft";
+    "net.minecraft.client.audio.SoundEngine" = "minecraft";
+    "net.minecraft.client.audio.SoundHandler" = "minecraft";
+    "net.minecraft.client.audio.SoundSystem" = "minecraft";
+    "net.minecraft.client.gui.NewChatGui" = "minecraft";
+    "net.minecraft.client.gui.screen.ConnectingScreen" = "minecraft";
+    "net.minecraft.client.Minecraft" = "minecraft";
+    "net.minecraft.client.network.play.ClientPlayNetHandler" = "minecraft";
+    "net.minecraft.client.renderer.model.ModelBakery" = "minecraft";
+    "net.minecraft.client.renderer.texture.AtlasTexture" = "minecraft";
+    "net.minecraft.command.arguments.ArgumentTypes" = "minecraft";
+    "net.minecraft.entity.EntityType" = "minecraft";
+    "net.minecraft.network.datasync.EntityDataManager" = "minecraft";
+    "net.minecraft.resources.SimpleReloadableResourceManager" = "minecraft";
+    "net.minecraft.world.biome.Biome" = "minecraft";
+    "net.minecraft.world.storage.loot.LootTableManager" = "minecraft";
+    "net.minecraftforge.common.BiomeDictionary" = "forge";
+    "net.minecraftforge.common.DimensionManager" = "forge";
+    "net.minecraftforge.common.ForgeConfigSpec" = "forge";
+    "net.minecraftforge.common.ForgeMod" = "forge";
+    "net.minecraftforge.common.MinecraftForge" = "forge";
+    "net.minecraftforge.coremod.CoreMod.astralsorcery" = "forge";
+    "net.minecraftforge.coremod.CoreMod.immersiveengineering" = "forge";
+    "net.minecraftforge.coremod.CoreMod.observerlib" = "forge";
+    "net.minecraftforge.eventbus.EventBus" = "forge";
+    "net.minecraftforge.fml.DeferredWorkQueue" = "forge";
+    "net.minecraftforge.fml.loading.FixSSL" = "forge";
+    "net.minecraftforge.fml.network.FMLHandshakeHandler" = "forge";
+    "net.minecraftforge.fml.network.NetworkHooks" = "forge";
+    "net.minecraftforge.fml.VersionChecker" = "forge";
+    "net.minecraftforge.registries.ForgeRegistry" = "forge";
+    "net.minecraftforge.registries.GameData" = "forge";
+    "net.silentchaos512.utils.config.ConfigSpecWrapper" = "silentlib";
+    "Obfuscate" = "obfuscate";
+    "occultism" = "occultism";
+    "paintings" = "paintings";
+    "patchouli" = "patchouli";
+    "PluginManager" = "misc";
+    "powah" = "powah";
+    "projecte" = "projecte";
+    "psi" = "psi";
+    "quark" = "quark";
+    "se.mickelus.tetra.client.model.ModularModelLoader" = "tetra";
+    "se.mickelus.tetra.data.DataStore" = "tetra";
+    "se.mickelus.tetra.data.MergingDataStore" = "tetra";
+    "Silent Gear" = "silentgear";
+    "Silent's Gems" = "silentgems";
+    "strange" = "strange";
+    "structurize" = "structurize";
+    "tellme" = "tellme";
+    "TerraForged" = "terraforged";
+    "thelm.jaopca.data.DataCollector" = "jaopca";
+    "thelm.jaopca.localization.LocalizationRepoHandler" = "jaopca";
+    "thelm.jaopca.materials.MaterialHandler" = "jaopca";
+    "thelm.jaopca.modules.ModuleHandler" = "jaopca";
+    "tombstone" = "tombstone";
+    "tv.mapper.embellishcraft.EmbellishCraft" = "embellishcraft";
+    "tv.mapper.embellishcraftbop.EmbellishCraftBOP" = "embellishcraftbop";
+    "tv.mapper.mapperbase.MapperBase" = "mapperbase";
+    "tv.mapper.roadstuff.RoadStuff" = "roadstuff";
+    "Waterworks" = "waterworks";
+    "wile.engineersdecor.ModEngineersDecor" = "modengineersdecor";
+    "yalter.mousetweaks.Logger:Log:6" = "mousetweaks"
+}
+function Get-ModId {
+    param (
+        [string]$ModName
+    )
+    if ($modIdMapping.ContainsKey($ModName)) {
+        return $modIdMapping[$ModName]
+    }
+    # Special case mod names
+    if ($ModName.StartsWith("com.endlesnights.torchslabsmod.")) {
+        $modIdMapping.Add($ModName, "torchslabsmod");
+        return "torchslabsmod"
+    }
+    if ($ModName.StartsWith("ejektaflex.bountiful.")) {
+        $modIdMapping.Add($ModName, "bountiful");
+        return "bountiful"
+    }
+    if ($ModName.StartsWith("org.antlr.v4.runtime.")) {
+        $modIdMapping.Add($ModName, "misc");
+        return "misc"
+    }
+    if ($ModName.StartsWith("se.mickelus.mgui.")) {
+        $modIdMapping.Add($ModName, "mgui");
+        return "mgui"
+    }
+    if ($ModName.StartsWith("xaero.")) {
+        $modIdMapping.Add($ModName, "xaero");
+        return "xaero"
+    }
+    # Didn't find a match
+    $modIdMapping.Add($ModName, "unknown");
+    return "unknown"
+}
+
+$regEx = "\[(\d{2}\w{3}\d{4} \d{2}:\d{2}:\d{2}\.\d{3})\] \[([^/]*)\/([^\]]*)\] (?:\[STDERR\/\]: )?(?:\[STDOUT\/\]: )?\[([^/]*)\/?([^\]]*)?\]: (.*)"
+# Capture Group Positions (Some may not be used but are available if needed and for additional documentation)
+$cgTimestamp = 1
+$cgThreadName = 2
+$cgSeverity = 3
+$cgModName = 4
+$cgLogName = 5
+$cgMessage = 6
+
+$textStream = New-Object System.IO.StreamReader -Arg $LogFilePath
+$currentModName = "forge"
+
+while (-not ($textStream.EndOfStream)) {
+    $line = $textStream.ReadLine()
+    # Check for new section
+    if ($line -match $regEx) {
+        # New section - get mod name
+        $currentModName = Get-ModId $Matches[$cgModName]
+    }
+    $line | Out-File "$DestinationDirectory\$currentModName.log" -Encoding ascii -Append
+}
+$textStream.Close()
+$modIdMapping.GetEnumerator() | Select-Object -Property Key,Value | Export-Csv -Path "$DestinationDirectory\!modlist.log" -NoTypeInformation -Encoding ascii
