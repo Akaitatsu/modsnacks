2dc6863accb4c7da0128ad46f7d39cb599bde3b9
Akaitatsu <bryant@mindfair.com>
2020-11-30 18:52:28 -0500

    Fixed script picking up the "amounts" file

--- analytics/Get-DuplicateItemData.ps1
+++ analytics/Get-DuplicateItemData.ps1
@@ -12,7 +12,7 @@ if (-not (Test-Path $itemDataPath)) {
     Write-Host "No Block Stats files were found in dump path '$CrafterDumperOutputPath'." -ForegroundColor Red
     Exit 1
 }
-$items = Get-ChildItem $itemDataPath | Sort-Object LastWriteTime | Select-Object -last 1 | Get-Content | ConvertFrom-Csv
+$items = Get-ChildItem $itemDataPath -Exclude "item_stacks_*_amounts.csv" | Sort-Object LastWriteTime | Select-Object -last 1 | Get-Content | ConvertFrom-Csv
 $duplicates = $items | Group-Object -Property "Display name" | Where-Object { $_.Count -gt 1 } | Sort-Object -Property "Name"
 $duplicates
 
