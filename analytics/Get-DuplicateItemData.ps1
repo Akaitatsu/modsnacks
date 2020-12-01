4e0941c8c42c0a9953a40cc195510301fdc01dae
Akaitatsu <bryant@mindfair.com>
2020-12-01 00:33:23 -0500

    Removed duplicates from the same mod

--- analytics/Get-DuplicateItemData.ps1
+++ analytics/Get-DuplicateItemData.ps1
@@ -16,6 +16,7 @@ if (-not (Test-Path $itemDataPath)) {
 }
 $items = Get-ChildItem $itemDataPath -Exclude "item_stacks_*_amounts.csv" | Sort-Object LastWriteTime | Select-Object -last 1 | Get-Content -Raw | ConvertFrom-Csv
 $duplicateGroups = $items | Group-Object -Property "Display Name" | Where-Object { $_.Count -gt 1 } | Sort-Object -Property "Name"
+$items = $null
 [System.Collections.ArrayList]$duplicateItems = @()
 "Display Name,Mod ID,Item ID" | Out-File $duplicatesFilePath -Encoding ascii
 foreach ($duplicateItem in $duplicateGroups) {
@@ -29,6 +30,22 @@ foreach ($duplicateItem in $duplicateGroups) {
         }) | Out-Null
     }
 }
+$duplicateGroups = $null
+# Remove duplicates where all the items belong to the same mod
+$duplicateGroups = $duplicateItems | Group-Object -Property DisplayName
+[Collections.Generic.List[string]]$itemsToDelete = @()
+foreach ($duplicateItem in $duplicateGroups) {
+    if (($duplicateItem.Group | Group-Object -Property ModID).Name.Count -eq 1) {
+        $itemsToDelete.Add($duplicateItem.Group[0].DisplayName) | Out-Null
+    }
+}
+$duplicateItems.Count
+for ($i = $duplicateItems.Count - 1; $i -ge 0; $i--) {
+    if ($itemsToDelete.Contains($duplicateItems[$i].DisplayName)) {
+        $duplicateItems.RemoveAt($i)
+    }
+}
+$duplicateItems.Count
 $duplicateItems | ConvertTo-Csv -NoTypeInformation | Out-File $duplicatesFilePath -Encoding ascii
 #if (Test-Path $duplicatesFilePath) { Remove-Item $duplicatesFilePath -Force }
 Pop-Location
